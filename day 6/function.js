b0ccba04309fb97ee0c46144183c32f59f4d91e0
Saabika Roshni <saabikaroshni@gmail.com>
2025-02-16 20:50:56 +0530

    task6

--- day 6/function.js
+++ day 6/function.js
@@ -1,4 +1,16 @@
-function testFunction(){
-    console.log("This is test function")
+function testFunction1(){
+    console.log("This is function without parameter without return type")
 }
-testFunction()
+testFunction1()
+function testFunction2(x){
+    console.log("This is function with parameter without return type",x)
+}
+testFunction2(5)
+function testFunction3(){
+    return "This is function with parameter with return type"
+}
+console.log(testFunction3());
+function testFunction4(x){
+    return x
+}
+console.log("This is function with parameter with return type",testFunction4(5));
