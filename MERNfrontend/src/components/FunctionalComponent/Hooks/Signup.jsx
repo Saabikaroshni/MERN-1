c3cfa740154609ff25499a8908feeb400c73aa26
Saabika Roshni S <saabikaroshni@gmail.com>
2025-02-27 09:01:52 +0530

    backend

--- MERNfrontend/src/components/FunctionalComponent/Hooks/Signup.jsx
+++ MERNfrontend/src/components/FunctionalComponent/Hooks/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
   const [phoneNumber,setnum]=useState(0);
   const handlesignup =async (event)=>{
     event.preventDefault()
-    const req=await axios.post("https://mern-1-w5us.onrender.com/signup",{
+    const req=await axios.post("https://mern-1-w5us.onrender.com/Signup",{
       firstName:firstName,lastName:lastName,email:email,password:password,phoneNumber:phoneNumber
     });
     const message=req.data.message
